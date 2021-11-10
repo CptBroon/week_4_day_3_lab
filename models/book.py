cf729dfd2061d466f9a7e0fe0ffd766bc1885e66
Graeme Brown <graemebrown1874@gmail.com>
2021-11-10 15:20:55 +0000

    models created

---
+++ models/book.py
@@ -0,0 +1,8 @@
+class Book:
+    
+    def __init__(self, title, release_year, author, id = None):
+        self.title = title
+        self.release_year = release_year
+        self.author = author
+        self.id = id
+        
